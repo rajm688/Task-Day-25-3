66c4ac27cad1010bbfe8bd95884ba1d1a2d9e4b0
Rajkumar Murugesan <90443069+rajm688@users.noreply.github.com>
2021-12-10 11:16:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-let url = "https://api-mobilespecs.azharimm.site/v2/brands"
-const fetchData = async ()=>{
+let url = "https://api-mobilespecs.azharimm.site/v2/brands" // assigning  url to an variable
+const fetchData = async ()=>{ //using async and await with fetch to fetching data
    const response = await fetch(url);
     const datas = await response.json();
-        for(let i=0; i< datas.data.length; i++){
-            let main = document.createElement("div")
+        for(let i=0; i< datas.data.length; i++){ // looping through the array elements
+            let main = document.createElement("div") //creating container elements
             main.className="container"
+           //getting necessery data
              let brandName =datas.data[i].brand_name;
              let brandSlug = datas.data[i].brand_slug;
              let deviceCount = datas.data[i].device_count;
              let details = datas.data[i].detail;
+           //inserting data to the div element
              main.innerHTML =`<strong>Brand Name :</strong><i> ${brandName} <i> <br>
              <strong>Brand Name :</strong><i> ${brandSlug}</i><br>
              <strong>Device Count : </strong><i>${deviceCount}</i><br>
